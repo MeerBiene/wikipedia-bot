88a5aa8831bba7c35a3f6cb3ce1e79601746c1e1
MeerBiene <b.buhles@gmail.com>
2021-01-23 01:05:46 +0100

    started interactionhandler

---
+++ modules/interactions.js
@@ -0,0 +1,14 @@
+module.exports = (client) => {
+    client.slash = {
+        commands = {
+            // check if the command exists
+            get: async () => {
+
+            },
+            // run the command
+            run: async () => {
+
+            }
+        }
+    }
+}
